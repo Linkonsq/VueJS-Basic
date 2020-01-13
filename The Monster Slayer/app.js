5fc3411802ef8e0ad6062b44bab8eb118bdefe98
Linkon Siddique <linkon.sq48@hotmail.com>
2020-01-14 00:56:17 +0600

    All button functionality working

--- The Monster Slayer/app.js
+++ The Monster Slayer/app.js
@@ -4,5 +4,64 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false
+    },
+    methods: {
+        startGame: function () {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        },
+        attack: function () {
+            this.monsterHealth -= this.calcualteDamage(3, 10);
+
+            if (this.checkWin()) {
+                return; //end here. never execute below code
+            }
+
+            this.mosnterAttacks();
+        },
+        specialAttack: function () {
+            this.monsterHealth -= this.calcualteDamage(10, 20);
+            if (this.checkWin()) {
+                return; //end here. never execute below code
+            }
+            this.mosnterAttacks();
+        },
+        heal: function () {
+            if(this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+            this.mosnterAttacks();
+        },
+        giveUp: function () {
+            this.gameIsRunning = false;
+        },
+        mosnterAttacks: function() {
+            this.playerHealth -= this.calcualteDamage(5, 12);
+            this.checkWin();
+        },
+        calcualteDamage: function (min, max) {
+            return damage = Math.max(Math.floor(Math.random() * max) + 1, min);//Math.random() is between 0 and 1
+        },
+        checkWin: function () {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You won! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You lost! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
+        }
     }
 });
